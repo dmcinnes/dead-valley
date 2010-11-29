3d71e089e84d3ffdd07be13bc5f33fc61f0eae5e
Doug McInnes <doug@dougmcinnes.com>
2010-11-28 23:36:24 -0800

    made matrix a bit more performant

--- matrix.js
+++ matrix.js
@@ -2,22 +2,27 @@
 
 define(function () {
   return function (rows, columns) {
-    var i, j;
+    var i, j, k, rad, sin, cos, vector;
+
     this.data = new Array(rows);
     for (i = 0; i < rows; i++) {
       this.data[i] = new Array(columns);
     }
 
     this.configure = function (rot, scale, transx, transy) {
-      var rad = (rot * Math.PI)/180;
-      var sin = Math.sin(rad) * scale;
-      var cos = Math.cos(rad) * scale;
-      this.set(cos, -sin, transx,
-               sin,  cos, transy);
+      rad = (rot * Math.PI)/180.0;
+      sin = Math.sin(rad) * scale;
+      cos = Math.cos(rad) * scale;
+      this.data[0][0] = cos;
+      this.data[0][1] = -sin;
+      this.data[0][2] = transx;
+      this.data[1][0] = sin;
+      this.data[1][1] = cos;
+      this.data[1][2] = transy;
     };
 
     this.set = function () {
-      var k = 0;
+      k = 0;
       for (i = 0; i < rows; i++) {
         for (j = 0; j < columns; j++) {
           this.data[i][j] = arguments[k];
@@ -27,7 +32,7 @@ define(function () {
     }
 
     this.multiply = function () {
-      var vector = new Array(rows);
+      vector = new Array(rows);
       for (i = 0; i < rows; i++) {
         vector[i] = 0;
         for (j = 0; j < columns; j++) {
