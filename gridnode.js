cddd613dcb52b505e096d486f64388bd5a9931c4
Doug McInnes <doug@dougmcinnes.com>
2010-12-30 22:10:25 -0800

    now defining GridNode methods as prototypes
    load times sped up considerably since we're not creating methods each
    time a node is created

--- gridnode.js
+++ gridnode.js
@@ -21,96 +21,92 @@ define(["game"], function (game) {
 
     this.domNode = null;
 
-    this.dupe = {
-      horizontal: null,
-      vertical:   null
-    };
+  };
 
-    this.enter = function (sprite) {
-      sprite.nextSprite = this.nextSprite;
-      this.nextSprite = sprite;
-    };
+  GridNode.prototype.enter = function (sprite) {
+    sprite.nextSprite = this.nextSprite;
+    this.nextSprite = sprite;
+  };
 
-    this.leave = function (sprite) {
-      var ref = this;
-      while (ref && (ref.nextSprite != sprite)) {
-        ref = ref.nextSprite;
-      }
-      if (ref) {
-        ref.nextSprite = sprite.nextSprite;
-        sprite.nextSprite = null;
-      }
-    };
+  GridNode.prototype.leave = function (sprite) {
+    var ref = this;
+    while (ref && (ref.nextSprite != sprite)) {
+      ref = ref.nextSprite;
+    }
+    if (ref) {
+      ref.nextSprite = sprite.nextSprite;
+      sprite.nextSprite = null;
+    }
+  };
 
-    this.eachSprite = function(sprite, callback) {
-      var ref = this;
-      while (ref.nextSprite) {
-        ref = ref.nextSprite;
-        callback.call(sprite, ref);
-      }
-    };
+  GridNode.prototype.eachSprite = function(sprite, callback) {
+    var ref = this;
+    while (ref.nextSprite) {
+      ref = ref.nextSprite;
+      callback.call(sprite, ref);
+    }
+  };
 
-    this.isEmpty = function (collidables) {
-      var empty = true;
-      var ref = this;
-      while (ref.nextSprite) {
-        ref = ref.nextSprite;
-        empty = !ref.visible || collidables.indexOf(ref.name) == -1
-        if (!empty) break;
-      }
-      return empty;
-    };
+  GridNode.prototype.isEmpty = function (collidables) {
+    var empty = true;
+    var ref = this;
+    while (ref.nextSprite) {
+      ref = ref.nextSprite;
+      empty = !ref.visible || collidables.indexOf(ref.name) == -1
+      if (!empty) break;
+    }
+    return empty;
+  };
 
-    this.render = function (delta, x, y) {
-      // nothing to render, return
-      if (this.tileOffset == 0) return;
+  GridNode.prototype.render = function (delta, x, y) {
+    // nothing to render, return
+    if (this.tileOffset == 0) return;
 
-      if (x < -game.gridSize || y < -game.gridSize ||
-          x > game.canvasWidth ||
-          y > game.canvasHeight) {
-        // outside of the view
-        if (this.domNode) this.freeDomNode();
-      } else {
-        if (!this.domNode) this.obtainDomNode();
-        this.domNode.css({left:x, top:y});
-      }
-    };
+    if (x < -game.gridSize || y < -game.gridSize ||
+        x > game.canvasWidth ||
+        y > game.canvasHeight) {
+      // outside of the view
+      if (this.domNode) this.freeDomNode();
+    } else {
+      if (!this.domNode) this.obtainDomNode();
+      this.domNode.css({left:x, top:y});
+    }
+  };
 
-    this.obtainDomNode = function () {
-      if (this.map.freeNodes.length) {
-        this.domNode = this.map.freeNodes.pop();
-        this.domNode.css({'background-position':game.gridSize * this.tileOffset+' 0px'}).show();
-      } else {
-        this.domNode = $('<div/>', {'class':'tile'}).css({'background-position':game.gridSize * this.tileOffset+' 0px'});
-        background.append(this.domNode);
-      }
-    };
+  GridNode.prototype.obtainDomNode = function () {
+    if (this.map.freeNodes.length) {
+      this.domNode = this.map.freeNodes.pop();
+      this.domNode.css({'background-position':game.gridSize * this.tileOffset+' 0px'}).show();
+    } else {
+      this.domNode = $('<div/>', {'class':'tile'}).css({'background-position':game.gridSize * this.tileOffset+' 0px'});
+      background.append(this.domNode);
+    }
+  };
 
-    this.freeDomNode = function (delta) {
-      this.domNode.hide();
-      this.map.freeNodes.push(this.domNode);
-      this.domNode = null;
-    };
+  GridNode.prototype.freeDomNode = function (delta) {
+    this.domNode.hide();
+    this.map.freeNodes.push(this.domNode);
+    this.domNode = null;
+  };
 
-    this.nearby = function () {
-      return _([this,
-                this.north,
-                this.south,
-                this.east,
-                this.west,
-                this.north.east,
-                this.north.west,
-                this.south.east,
-                this.south.west]).chain().map(function (n) {
-                  var spr = n.nextSprite;
-                  var out = [];
-                  while (spr) {
-                    out.push(spr);
-                    spr = spr.nextSprite;
-                  }
-                  return out;
-              }).flatten().value();
-    };
+  GridNode.prototype.nearby = function () {
+    return _([this,
+              this.north,
+              this.south,
+              this.east,
+              this.west,
+              this.north.east,
+              this.north.west,
+              this.south.east,
+              this.south.west]).chain().map(function (n) {
+                var spr = n.nextSprite;
+                var out = [];
+                while (spr) {
+                  out.push(spr);
+                  spr = spr.nextSprite;
+                }
+                return out;
+            }).flatten().value();
   };
 
   return GridNode;
