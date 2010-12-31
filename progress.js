24153ba5520794602eb73e1d9047b4e44e048d81
Doug McInnes <doug@dougmcinnes.com>
2010-12-30 22:15:38 -0800

    progress bar

---
+++ progress.js
@@ -0,0 +1,41 @@
+// Progress Bar
+
+define(function () {
+  
+  var container = $('#progress');
+  var slider    = $('#progress-slider');
+
+  var total = 100;
+  var current = 0;
+
+  var callback = null;
+
+  var updateSlider = function () {
+    slider.width((100 * current / total) + '%');
+    if (current >= total) {
+      container.hide();
+      callback && callback();
+    }
+  };
+
+  return {
+    setTotal: function (newTotal, cb) {
+      callback = cb;
+      current = 0;
+      total = newTotal;
+      updateSlider();
+      container.show();
+    },
+
+    increment: function () {
+      current++;
+      updateSlider();
+      return current;
+    },
+
+    current: function () {
+      return current;
+    }
+  };
+});
+
