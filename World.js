59aba51095763fd585c3dca1beb2cc63653999f5
Doug McInnes <doug@dougmcinnes.com>
2011-03-06 18:49:48 -0800

    using localStorage for tile data

--- World.js
+++ World.js
@@ -2,20 +2,20 @@
 
 define([], function () {
 
-  // just gonna hold this here for now
-  // TODO use storage
-  var worldMap = {};
+  // save the bits in localStorage
+  var worldMap = localStorage;
 
   var World = {
     setTiles: function (position, tiles) {
       // console.log("Set ", position.toString());
       tiles.position = position;
-      worldMap[position.toString()] = tiles;
+      worldMap[position.toString()] = JSON.stringify(tiles);
     },
 
     getTiles: function (position) {
-      // console.log("Get ", position.toString(), worldMap[position.toString()] !== undefined);
-      return worldMap[position.toString()];
+      var data = worldMap[position.toString()];
+      // console.log("Get ", position.toString(), data !== undefined);
+      return data && JSON.parse(data);
     },
 
     getSurroundingRoads: function (position) {
