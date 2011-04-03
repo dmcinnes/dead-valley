67ed4be3d910b25240fb1bee5be95eda55737ba1
Doug McInnes <doug@dougmcinnes.com>
2011-04-03 00:21:01 -0700

    that comment is way out of date

--- mapworker.js
+++ mapworker.js
@@ -1,29 +1,5 @@
 // a Web Worker for filling out the map
 
-// Existing tiles:
-// TODO move these into a constants file
-//
-// 0 blank
-// 1 dirt
-// 2 scrub
-// 3 road edge
-// 4 asphalt
-// 5 road double yellow lines
-// 6 road dashed and solid yellow lines
-// 7 building corner (lower left)
-// 8 building side (bottom)
-
-// our 'meta' map tiles are 128 level tiles wide and 64 tall
-// they are added to the map either horizontally or vertically
-//
-// So what do we have
-//
-// horizontal road
-// vertical road
-// crossroads
-// curve left
-// curve right
-
 // so we can use tileMarshal even though it's defined
 // with requirejs
 var tileMarshal;
