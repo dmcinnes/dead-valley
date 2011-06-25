0a0a6a67f4bbaf4658f4a5c7da43eeb96f26c9b1
Doug McInnes <doug@dougmcinnes.com>
2011-06-25 00:35:37 -0700

    add pause overlay that disables clicks

--- hud/Pause.js
+++ hud/Pause.js
@@ -1,9 +1,16 @@
 define(['game'], function (game) {
-  var parseNode = $('#pause');
+  var overlay = $('#pause-overlay');
+
+  var mouseEvents = "click, dblclick, mousedown, mouseup, mousemove, mouseover, mouseout, mouseenter, mouseleave";
+
+  overlay.bind(mouseEvents, function (e) {
+    e.stopImmediatePropagation();
+    return false;
+  });
 
   game.events.subscribe('pause', function () {
-    parseNode.show();
+    overlay.show();
   }).subscribe('play', function () {
-    parseNode.hide();
+    overlay.hide();
   });
 });
