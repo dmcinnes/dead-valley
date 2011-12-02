b244a78de55068cf99cd61c64bdb60746eda226d
Doug McInnes <doug@dougmcinnes.com>
2011-12-01 20:46:55 -0800

    save headlight on state

--- Car.js
+++ Car.js
@@ -100,7 +100,9 @@ define(["Game",
   Car.prototype = new RigidBody();
 
   Car.prototype.draw = function () {
-    if (!this.visible) return;
+    if (!this.visible || !this.imageData) {
+      return;
+    }
 
     // dead!
     if (this.health <= 0) {
@@ -375,12 +377,13 @@ define(["Game",
 
   Car.prototype.saveMetadata = function () {
     var data = Sprite.prototype.saveMetadata.call(this);
-    data.inventory   = this.inventory && this.inventory.saveMetadata();
-    data.currentFuel = this.currentFuel;
-    data.health      = this.health;
-    data.canSmoke    = false; // car stops smoking after it is saved off
-    data.headlights  = this.headlights;
-    data.taillights  = this.taillights;
+    data.inventory    = this.inventory && this.inventory.saveMetadata();
+    data.currentFuel  = this.currentFuel;
+    data.health       = this.health;
+    data.canSmoke     = false; // car stops smoking after it is saved off
+    data.headlights   = this.headlights;
+    data.taillights   = this.taillights;
+    data.headlightsOn = this.headlightsOn;
     return data;
   };
 
